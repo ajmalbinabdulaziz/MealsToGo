51673b630c82869e3d5b0bd723181925d553a9d5
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2021-09-15 13:11:14 +0530

    Loaded fonts- oswald and lato

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,21 +1,23 @@
-
 import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
 const Title = styled(Text)`
-  padding: 16px;
-  color: red;
+  font-family: ${(props) => props.theme.fonts.body}
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  color: ${(props) => props.theme.colors.bg.primary};
+  
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px; 
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.bg.primary};
+
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -38,4 +40,3 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     </RestaurantCard>
   );
 };
-
